f4f5244785974894fb7623f5eb6120ecf9798c24
Pael Cadia <paelcadia@Gmail.com>
2026-02-22 03:08:24 +0700

    Portofolio WannaBe Pael

---
+++ script.js
@@ -0,0 +1,8 @@
+let Nama = document.getElementsByClassName("intro h3");
+console.log(Nama);
+
+let Perkenalan = document.querySelector("intro p");
+console.log(Perkenalan);
+
+let Job = document.getElementsByClassName("intro");
+console.log(Job);
