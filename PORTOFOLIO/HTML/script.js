8b82a1344d0bba646d2083b48e6cc30da3c9b75e
Pael Cadia <159473670+paelcadia@users.noreply.github.com>
2026-02-22 03:07:28 +0700

    Delete PORTOFOLIO/HTML directory

--- PORTOFOLIO/HTML/script.js
+++
@@ -1,8 +0,0 @@
-let Nama = document.getElementsByClassName("intro h3");
-console.log(Nama);
-
-let Perkenalan = document.querySelector("intro p");
-console.log(Perkenalan);
-
-let Job = document.getElementsByClassName("intro");
-console.log(Job);
